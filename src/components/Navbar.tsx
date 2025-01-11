fb1213cff64136f6dc8bc5267eddc0f53f682bd3
harshith1019 <136225803+harshith1019@users.noreply.github.com>
2025-01-11 10:38:48 +0530

    Create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link active mx-2" aria-current="page" href="#">
+                Crypto Taxes
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active mx-2" href="#">
+                Free Tools
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active mx-2" href="#">
+                Resource Center
+              </a>
+            </li>
+            <li className="nav-item mx-2">
+              <button className="btn btn-primary">Get Started</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
