3fba41604aa667c2c26b31a590536b905626d7db
harshith1019 <136225803+harshith1019@users.noreply.github.com>
2025-01-11 23:02:21 +0530

    Create "You May Also Like"

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -42,7 +42,67 @@ const Footer = () => {
 
   return (
     <div className="footer bg-light py-3">
-      <h5 className="text-center mb-4">Trending Coins</h5>
+      <h5 className="text-center mb-4">You May Also Like</h5>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={10}
+        slidesPerView={3} // Adjust the number of slides shown
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+        style={{ padding: "0 1rem" }}
+      >
+        {coins.map((coin) => (
+          <SwiperSlide key={coin.id}>
+            <div
+              className="card d-flex flex-column align-items-center p-2"
+              style={{
+                width: "100%",
+                boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                borderRadius: "8px",
+              }}
+            >
+              {/* Thumbnail */}
+              <img
+                src={coin.thumb}
+                alt={coin.name}
+                className="img-fluid rounded-circle mb-2"
+                style={{ width: "40px", height: "40px" }}
+              />
+              {/* Symbol */}
+              <h6 className="text-uppercase">{coin.symbol}</h6>
+              {/* Price Change Percentage */}
+              <p
+                className={`${
+                  coin.data.price_change_percentage_24h.usd >= 0
+                    ? "text-success"
+                    : "text-danger"
+                }`}
+              >
+                {coin.data.price_change_percentage_24h.usd.toFixed(2)}%
+              </p>
+              {/* Sparkline */}
+              {coin.data.sparkline && (
+                <img
+                  src={coin.data.sparkline}
+                  alt={`${coin.name} sparkline`}
+                  className="img-fluid mt-2"
+                  style={{
+                    width: "100%",
+                    outline: "none",
+                    border: "none",
+                  }}
+                />
+              )}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <h5 className="text-center my-4">Trending Coins</h5>
       <Swiper
         modules={[Navigation, Pagination]}
         spaceBetween={10}
